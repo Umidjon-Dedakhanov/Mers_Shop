2d03285a3fec5e8612fa96a0cecf2d742ddadf6c
Diyorbek Olimov <diyorbekolimov2000@gmail.com>
2022-04-09 23:57:01 +0500

    added redux devtools extension

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,8 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducers from "../reducers";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const initialState = {};
-const store = createStore(rootReducers, initialState, applyMiddleware(thunk));
+const store = createStore(
+  rootReducers,
+  initialState,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export { store };
