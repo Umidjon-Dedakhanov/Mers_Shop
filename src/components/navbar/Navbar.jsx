b917a40f8cc8cc84235a5f1c5fce8986ad54c653
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2022-04-09 05:52:40 +0500

    select

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import { NavLink, useLocation } from "react-router-dom";
 
 import logo from "../../assets/home/logo.png";
@@ -23,9 +23,12 @@ export const Navbar = () => {
     }
   });
 
-  const { isAuthenticated } = useSelector(state => state.authReducer);
+  const { isAuthenticated } = useSelector((state) => state.authReducer);
   return (
-    <nav className={style.navbar + " " + scroll} style={pathname.includes("account") ? {background: "#333"} : null}>
+    <nav
+      className={style.navbar + " " + scroll}
+      style={pathname.includes("account") ? { background: "#333" } : null}
+    >
       <div className={style.navbar_container + " " + style.container}>
         <input type="checkbox" name="" id="" />
         <div className={style.hamburger_lines}>
@@ -33,39 +36,89 @@ export const Navbar = () => {
           <span className={style.line + " " + style.line2}></span>
           <span className={style.line + " " + style.line3}></span>
         </div>
+        <div className={style.navbar__select}>
+          <Select />
+          {isAuthenticated ? (
+            <img src={person} alt="" />
+          ) : (
+            <NavLink exact to={"/login"}>
+              Login
+            </NavLink>
+          )}
+        </div>
         <ul className={style.menu_items}>
           <li>
-            <NavLink exact className={style.item__inactive} activeClassName={style.item__active} to="/">Home</NavLink>
+            <NavLink
+              exact
+              className={style.item__inactive}
+              activeClassName={style.item__active}
+              to="/"
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <NavLink exact className={style.item__inactive} activeClassName={style.item__active}  to="/about">About Us</NavLink>
-            
+            <NavLink
+              exact
+              className={style.item__inactive}
+              activeClassName={style.item__active}
+              to="/about"
+            >
+              About Us
+            </NavLink>
           </li>
           <li>
-            <NavLink exact className={style.item__inactive} activeClassName={style.item__active}  to="/product">Products</NavLink>
+            <NavLink
+              exact
+              className={style.item__inactive}
+              activeClassName={style.item__active}
+              to="/product"
+            >
+              Products
+            </NavLink>
           </li>
           <li>
-            <NavLink exact className={style.item__inactive} activeClassName={style.item__active}  to="/new">News</NavLink>
+            <NavLink
+              exact
+              className={style.item__inactive}
+              activeClassName={style.item__active}
+              to="/new"
+            >
+              News
+            </NavLink>
           </li>
           <li>
-            <NavLink exact className={style.item__inactive} activeClassName={style.item__active}  to="/contact">Contact Us</NavLink>
+            <NavLink
+              exact
+              className={style.item__inactive}
+              activeClassName={style.item__active}
+              to="/contact"
+            >
+              Contact Us
+            </NavLink>
           </li>
-          <li className={style.forSearch}>
-          {window.innerWidth <= 769 ? null : <Search />}
-
-          </li>
-          <li>
+          {window.innerWidth <= 769 ? null : (
+            <li className={style.forSearch}>
+              <Search />
+            </li>
+          )}
+          <li className={style.navbar__selectLG}>
             <Select />
-            {isAuthenticated
-              ?<img src={person} alt="" />
-              : <NavLink exact to={'/login'}>Login</NavLink>
-            } 
+            {isAuthenticated ? (
+              <img src={person} alt="" />
+            ) : (
+              <NavLink exact to={"/login"}>
+                Login
+              </NavLink>
+            )}
           </li>
           <li>
             <Switch />
           </li>
         </ul>
-        <NavLink to={'/'}><img src={logo} alt="" className={style.logo} /></NavLink>
+        <NavLink to={"/"}>
+          <img src={logo} alt="" className={style.logo} />
+        </NavLink>
       </div>
     </nav>
   );
