0b0d1ad79fb62a2ead9c64d540ce08f8537044ad
REACT-DEVELOPER-IBROKHIM <ibrohim123451@gmail.com>
2022-04-25 23:27:54 +0500

    Test Order render API key value pairs misunderstanding inserted and updated keys are still coming null from back

--- src/routes/account/account-all/AccountAll.jsx
+++ src/routes/account/account-all/AccountAll.jsx
@@ -1,57 +1,29 @@
 import React from 'react';
 import classes from './AccountAll.module.css';
 import AccountProduct from '../account-product/AccountProduct';
-import productImage from "../../../assets/karzinka/product.png";
+// import productImage from "../../../assets/karzinka/product.png";
+import useAuthFetch from '../../../hooks/useAuthFetch';
+const ALL_BASKET_PRODUCTS_ENDPOINT = "clientMainsAPI";
 
 const AccountAll = () => {
-    const data = [
-        {
-          id: 0,
-          productImage: productImage,
-          productTitle: `Black/Red/Blue 1" Extra Flow Breather 1" Push In Vent Filter for Valve Cover`,
-          productRatings: 4,
-          productCost: 12.99,
-          productCount: 3
-        },
-        {
-          id: 1,
-          productImage: productImage,
-          productTitle: `Black/Red/Blue 1" Extra Flow Breather 1" Push In Vent Filter for Valve Cover`,
-          productRatings: 4,
-          productCost: 12.99,
-          productCount: 5
-        },
-        {
-            id: 2,
-            productImage: productImage,
-            productTitle: `Black/Red/Blue 1" Extra Flow Breather 1" Push In Vent Filter for Valve Cover`,
-            productRatings: 4,
-            productCost: 12.99,
-            productCount: 5
-          },
-          {
-            id: 3,
-            productImage: productImage,
-            productTitle: `Black/Red/Blue 1" Extra Flow Breather 1" Push In Vent Filter for Valve Cover`,
-            productRatings: 4,
-            productCost: 12.99,
-            productCount: 5
-          },
-          {
-            id: 4,
-            productImage: productImage,
-            productTitle: `Black/Red/Blue 1" Extra Flow Breather 1" Push In Vent Filter for Valve Cover`,
-            productRatings: 4,
-            productCost: 12.99,
-            productCount: 5
-          }
-      ]
+  const { data } = useAuthFetch(ALL_BASKET_PRODUCTS_ENDPOINT, null);
+    // const data2 = [
+    //     {
+    //       id: 0,
+    //       productImage: productImage,
+    //       productTitle: `Black/Red/Blue 1" Extra Flow Breather 1" Push In Vent Filter for Valve Cover`,
+    //       productRatings: 4,
+    //       productCost: 12.99,
+    //       productCount: 3
+    //     }
+    //   ]
+      console.log(data)
     return (
         <div className={classes.account__all}>
            <div className={classes.all__header}>
                 <h1>Неоплаченный</h1>
             </div>
-            <AccountProduct data={data} tableHeaders={["Продукты", "Количество", "Статус заказа", "Опции"]} />
+            <AccountProduct data={data[data.length - 1]?.basketMain} tableHeaders={["Продукты", "Количество", "Статус заказа", "Опции"]} />
         </div>
     )
 }
